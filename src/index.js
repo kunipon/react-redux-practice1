d9b55233668660175c3b2dfc7f0e768045ad5c80
kunipon <kunikunipon.0328@gmail.com>
2019-09-17 17:32:01 +0900

    implement delete feature

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
-import {Provider} from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import './index.css';
 import reducer from './reducers';
 
 import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
+import EventsShow from './components/events_show';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore( reducer, applyMiddleware(thunk) )
+const enhancer = process.env.NODE_ENV === 'development' ?
+  composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
+applyMiddleware(thunk)
+const store = createStore(reducer, enhancer)
 
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/events/new" component={EventsNew} />
-                <Route exact path="/" component={EventsIndex}></Route>
-            </Switch>
-        </BrowserRouter>
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/events/new" component={EventsNew} />
+        <Route path="/events/:id" component={EventsShow} />
+        <Route exact path="/" component={EventsIndex}></Route>
+        <Route exact path="/events" component={EventsIndex}></Route>
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,12 +1,18 @@
 import _ from 'lodash'
-import {READ_EVENTS} from '../actions'
+import {READ_EVENTS, DELETE_EVENT, READ_EVENT, UPDATE_EVENT, CREATE_EVENT} from '../actions'
 
 export default (events={}, action) => {
     switch (action.type) {
         case READ_EVENTS:
             return _.mapKeys(action.response.data, 'id')
-        // case DECREMENT:
-        //     return {value: state.value - 1}
+        case READ_EVENT:
+        case CREATE_EVENT:
+        case UPDATE_EVENT:
+            const data = action.response.data
+            return { ...events, [data.id]: data }
+        case DELETE_EVENT:
+            delete events[action.id]
+            return { ...events }
         default:
             return events
     }
